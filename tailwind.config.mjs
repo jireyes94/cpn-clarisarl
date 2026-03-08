91db0c4576c8f881e1424eda952ac927632f4f73
Nacho <96269828+jireyes94@users.noreply.github.com>
2026-03-08 16:33:30 -0300

    cambio en paleta de colores, tipografias y copywritgth

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					olive: '#2F4F4F',
+					sand: '#E8DFC8',
+					cream: '#F7F5F2',
+					gold: '#C6A75E',
+				},
+			},
+			fontFamily: {
+				// Para el nombre y títulos principales
+				display: ['"Playfair Display"', 'serif'],
+				// Para subtítulos y cuerpo de texto
+				sans: ['"Montserrat"', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
